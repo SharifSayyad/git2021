325775fd470a154bfe6f1954f1640a00977537ad
SHARIF SAYYAD <36179831+SharifSayyad@users.noreply.github.com>
2022-07-26 14:43:15 +0530

    Update sharif.py

--- sharif.py
+++ sharif.py
@@ -1,18 +1,2 @@
-print('sharif')
-print('new line')
-print('AbA')
-
-def new_task():
-  "new method"
-
-
-def local_fun():
-    "this is local function"
-    pass
-
- def remote_fun():
-  """this is remote function"""
-  pass
-
-def nnnnnnnnnnn():
+def Helo_word():
   pass
